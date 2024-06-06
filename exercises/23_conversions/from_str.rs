8b2b4ea641152c198794275a78fa6226ad2a5300
Jackson Fair <jaxsofair@gmail.com>
2024-06-06 14:54:19 -0400

    Finished Rustlings
    Last activities were tough. I definitely need to get better at using match statements, especially in the context of error handling and propagation.

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -48,6 +46,20 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            Err(ParsePersonError::Empty)
+        } else {
+            let mut iter = s.split(",");
+            let name = iter.next().unwrap().to_string();
+            if name.is_empty() {
+                Err(ParsePersonError::NoName)?
+            };
+            let age = iter.next().ok_or(ParsePersonError::BadLen)?.parse().map_err(ParsePersonError::ParseInt)?;
+            if let Some(x) = iter.next() {
+                Err(ParsePersonError::BadLen)?
+            };
+            Ok(Person{name, age})
+        }
     }
 }
 
